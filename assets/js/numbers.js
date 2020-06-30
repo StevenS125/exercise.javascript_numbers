f9a05a3fcda7ac2cd10f1f444e65e3903cedb6bf
Steven Snyder <sts12589@gmail.com>
2020-06-30 15:34:00 -0400

    solved but simplified

--- assets/js/numbers.js
+++ assets/js/numbers.js
@@ -1,8 +1,5 @@
 function compute(expression) {
-    var total = 0;
-    var s = expression
-    // Seperate values with operators
-s = s.replace(/\s/g, '').match(/[+\-*/]?([0-9\.\s]+)/g) || [];
+
 return eval(expression)
 
 }
