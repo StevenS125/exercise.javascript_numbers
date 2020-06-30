40c9b5b9836b2ccc7e2b9f226928030e350326fa
Steven Snyder <sts12589@gmail.com>
2020-06-30 16:20:26 -0400

    need to address order of operations issue

--- assets/js/numbers.js
+++ assets/js/numbers.js
@@ -1,5 +1,26 @@
 function compute(expression) {
+    var total = 0;
+    var s = expression
+    // Seperate values with operators
+s = s.replace(/\s/g, '').match(/[+\-*/]?([0-9\.\s]+)/g) || [];
+s.forEach(element => {
+    if (element.indexOf('*') != -1){
+        element = element.slice(1)
+        console.log(element)
+        total = total * parseInt(element)
+    }else if (element.indexOf('/') != -1){
+        element = element.slice(1)
+        console.log(element)
+        total = total / parseInt(element)
+    } else {
+        total += parseFloat(element)
+    }
 
-return eval(expression)
+    console.log(element)
+
+});
+
+return total
+
 
 }
